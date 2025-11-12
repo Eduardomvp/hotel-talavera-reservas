15d3263456d52ae5bad084f0efaaeeeec2d0e29a
Eduardomvp <167665941+Eduardomvp@users.noreply.github.com>
2025-11-12 17:46:20 -0600

    Add reservation module with CRUD functions

---
+++ js/modules/reservations.js
@@ -0,0 +1,21 @@
+// reservations.js
+
+// Function to create a new reservation
+function createReservation(reservationData) {
+    // Code to create a reservation
+}
+
+// Function to read a reservation by ID
+function readReservation(reservationId) {
+    // Code to read a reservation
+}
+
+// Function to update an existing reservation
+function updateReservation(reservationId, updatedData) {
+    // Code to update a reservation
+}
+
+// Function to delete a reservation
+function deleteReservation(reservationId) {
+    // Code to delete a reservation
+}
