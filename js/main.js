9f7e9b9a9b23660f4159d624740e5c658d1924ae
Eduardomvp <167665941+Eduardomvp@users.noreply.github.com>
2025-11-12 17:46:15 -0600

    Create main JavaScript module for application initialization

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// main.js
+// Entry point for the Hotel Talavera Reservas application
+
+function init() {
+    console.log('Initializing Hotel Talavera Reservas application...');
+    // Add additional initialization code here
+}
+
+// Invoke the init function to start the application
+init();
